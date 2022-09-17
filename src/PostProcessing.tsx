9aeb403b96f7283679ec9cc3abffb7a21af0f0a1
Hendrik Mans <hendrik@mans.de>
2022-09-17 19:06:04 +0200

    Upgrade

--- src/PostProcessing.tsx
+++ src/PostProcessing.tsx
@@ -1,4 +1,5 @@
 import { useTexture } from "@react-three/drei"
+import { useControls } from "leva"
 import * as RC from "render-composer"
 import { useStore } from "statery"
 import { store } from "./state"
@@ -7,16 +8,25 @@ export const PostProcessing = () => {
   const { sun } = useStore(store)
   const texture = useTexture("/textures/lensdirt.jpg")
 
+  const controls = useControls("Post Processing", {
+    smaa: true,
+    bloom: true,
+    godRays: true,
+    lensDirt: true
+  })
+
   return (
     <RC.EffectPass>
-      <RC.SMAAEffect />
-      <RC.SelectiveBloomEffect
-        intensity={3}
-        luminanceSmoothing={0.3}
-        luminanceThreshold={0.8}
-      />
-      {sun && <RC.GodRaysEffect lightSource={sun} />}
-      <RC.LensDirtEffect texture={texture} />
+      {controls.smaa && <RC.SMAAEffect />}
+      {controls.bloom && (
+        <RC.SelectiveBloomEffect
+          intensity={3}
+          luminanceSmoothing={0.3}
+          luminanceThreshold={0.8}
+        />
+      )}
+      {controls.godRays && sun && <RC.GodRaysEffect lightSource={sun} />}
+      {controls.lensDirt && <RC.LensDirtEffect texture={texture} />}
     </RC.EffectPass>
   )
 }
